32408f43687a44cb11dd3bccdb1272540d07b44d
‘morganelcvz’ <morgane.lecalvez@outlook.fr>
2025-01-14 15:15:37 +0100

    ajout v1 design

--- js/script.js
+++ js/script.js
@@ -1,26 +1,24 @@
-fetch('https://api.openweathermap.org/data/2.5/forecast?q=Havre,fr&appid=ee6df9f0e15bebd128db31decab1af46')
-    .then(response => response.json())
-    .then(data => { 
-        console.log(data)
-         document.getElementById("todayMeteo").innerHTML = `
-                <div class="title">
-                  <span>${data.city.name}</span> <p>${data.dt_txt}</p>
-                </div>
-                <div class="degree">
-                 ${data.main.temp}
-                </div>
-            <div class="status">${data.weather.description}</div>
-            <div class="temp">${data.main.temp_min} / ${data.main.temp_max}</div>
-         `
-     })
-    .catch(error => console.error(error))
+// fetch('https://api.openweathermap.org/data/2.5/forecast?q=Havre&lang=fr&units=metric&appid=ee6df9f0e15bebd128db31decab1af46')
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data.list[0])
+//         console.log(data.list[0].main.temp)
 
- 
-//     <div class="title">
-//     <span>Le Havre</span> <p>Lundi 13 janvier 2025 / 18:45</p>
-// </div>
-// <div class="degree">
-//     13°
-// </div>
-// <div class="status">Dégagé</div>
-// <div class="temp">6° / 20°</div>
+
+
+//         document.getElementById("todayMeteo").innerHTML = `
+//                 <div class="title">
+//                   <span>${data.city.name}</span> <p>${data.list[0].dt_txt}</p>
+//                 </div>
+//                 <div class="degree">
+//                  ${data.list[0].data.main.temp}
+//                 </div>
+//             <div class="status">${data.list[0].data.weather.description}</div>
+//             <div class="temp">${data.list[0].data.main.temp_min} / ${data.list[0].data.main.temp_max}</div>
+//          `
+//     })
+//     .catch(error => console.error(error))
+
+
+
+
