d34a8cafcab7167f2bcf71353234a4ea650b6bc8
‘morganelcvz’ <morgane.lecalvez@outlook.fr>
2025-01-15 16:34:57 +0100

    ajout search

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-fetch('https://api.openweathermap.org/data/2.5/forecast?q=havre&lang=fr&units=metric&appid=ee6df9f0e15bebd128db31decab1af46')
+const url = new URLSearchParams(window.location.search);
+const city = url.get('city')
+
+if(city == null || city == ""){
+    city = 'Havre'
+}
+
+fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=fr&units=metric&appid=ee6df9f0e15bebd128db31decab1af46`)
     .then(response => response.json())
     .then(data => {
         moment.locale('fr')
@@ -19,20 +26,16 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q=havre&lang=fr&units=me
         console.log(data.list[3].main.temp_max)
         console.log(moment(data.list[3].dt_txt).format('dddd'))
 
-        // const today = new Date(data.list[0].dt_txt)
-
-        // const datefr = today.toLocaleDateString("fr")
-
         // Affichage Meteo du jour TOP 
         document.getElementById("todayMeteo").innerHTML = `
                 <div class="title">
                   <span><i class="fa-solid fa-location-dot"></i> ${data.city.name}</span> <p>${moment(data.list[0].dt_txt).format('LLLL')}</p>
                 </div>
                 <div class="degree">
-                 ${(data.list[0].main.temp).toPrecision(1)}°c
+                 ${(data.list[0].main.temp).toPrecision(2)}°c
                 </div>
             <div class="status">${data.list[0].weather[0].description}</div>
-            <div class="temp">${(data.list[0].main.temp_min).toPrecision(1)}° / ${(data.list[0].main.temp_max).toPrecision(1)}°</div>
+            <div class="temp">${(data.list[0].main.temp_min).toPrecision(2)}° / ${(data.list[0].main.temp_max).toPrecision(2)}°</div>
          `
 
         // Affichage Meteo du jour Bottom 
@@ -40,7 +43,7 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q=havre&lang=fr&units=me
               <div class="top">
         <div class="box">
           <p>Ressenti</p>
-          <span>${(data.list[0].main.feels_like).toPrecision(1)}°c</span>
+          <span>${(data.list[0].main.feels_like).toPrecision(2)}°c</span>
         </div>
         <div class="box">
           <p>Humidité</p>
