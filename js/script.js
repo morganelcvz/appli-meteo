d2071b32ba4b7287da45d4aea0dafb6ccd8b6e52
‘morganelcvz’ <morgane.lecalvez@outlook.fr>
2025-01-17 11:49:36 +0100

    ajout appli terminée V2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
+// recherche city
+
 const url = new URLSearchParams(window.location.search);
 let city = url.get('city')
 
 if (city == null || city == "") {
     city = 'Havre'
 } 
+
+// message erreur : ville non trouvée 
 
 const object = {
   cod: "404",
@@ -12,8 +16,12 @@ const object = {
 
 if (object.cod == "404") {
   document.getElementById("todayMeteo").innerHTML = `
+  <div class="error">
   <h1>Erreur</h1>
   <h2>Cherchez une autre ville</h2>
+  <i class="fa-solid fa-cloud-showers-heavy"></i>
+  <i class="fa-regular fa-face-frown"></i>
+  </div>
   `
 }
 
@@ -38,6 +46,13 @@ fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=fr&units=
         console.log(data.list[3].main.temp_max)
         console.log(moment(data.list[3].dt_txt).format('dddd'))
 
+        const fiveDays = data.list.filter( date => date.dt_txt.split(' ')[1] == '12:00:00')
+
+        const otherDays = data.list.filter( date => date.dt_txt.split(' ')[1] == '12:00:00')
+
+        console.log(fiveDays)
+        console.log(otherDays)
+
         // Affichage Meteo du jour TOP 
         document.getElementById("todayMeteo").innerHTML = `
                 <div class="title">
@@ -49,6 +64,21 @@ fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=fr&units=
             <div class="status">${data.list[0].weather[0].description}</div>
             <div class="temp">${(data.list[0].main.temp_min).toPrecision(2)}° / ${(data.list[0].main.temp_max).toPrecision(2)}°</div>
          `
+
+        // Affichage des prévisions 5 jours 
+        document.getElementById("daysMeteo").innerHTML += `
+        <div class="date">
+        ${createFive(fiveDays)}
+        ${createOther(otherDays)}
+        <button class="btn plusday" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample"
+        aria-expanded="false" aria-controls="collapseExample">
+        <i class="fa-solid fa-bars"></i> plus de jours
+        </button>
+        <span class="preview">
+          <a href="#">prévisions 5 jours</a>
+        </span>
+        </div>
+        `
 
         // Affichage Meteo du jour Bottom 
         document.getElementById("todayPlus").innerHTML = `
@@ -73,30 +103,50 @@ fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=fr&units=
         </div>
       </div>
          `
-
-        //  function prochainJours(jours) {
-        //     let count = 5
-        //     jours.forEach(element =>
-        //         count-- 
-        //         if (count >= 0) {
-        //             document.getElementById("todayPlus").innerHTML = `
-        //                     <span class="day">
-        //                      <p>${moment(data.list[3].dt_txt).format('dddd')}</p> <img src="http://openweathermap.org/img/wn/03d@2x.png"> ${}° / 12°
-        //                     </span>
-        //             `
-        //         }
-        //     )
-        //  }
-
     })
     .catch(error => console.error(error))
 
-//     <span class="day">
-//     <p>Lundi</p> <img src="http://openweathermap.org/img/wn/03d@2x.png"> 9° / 12°
-//   </span>
-//   <span class="day">
-//     <p>Mardi</p> <img src="http://openweathermap.org/img/wn/10n@2x.png"> 10° / 15°
-//   </span>
-//   <span class="day">
-//     <p>Mercredi</p> <img src="http://openweathermap.org/img/wn/03d@2x.png"> 3° / 9°
-//   </span>
+
+    // Affichage des 3 premiers jours
+
+    function createFive(tableau) {
+      let myDiv = ''
+      let count = 3
+      tableau.forEach(element => {
+          count--
+          if (count >= 0) {
+              myDiv += `
+        <span class="day">
+          <p>${moment(element.dt_txt).format('dddd')}</p> <img src="http://openweathermap.org/img/wn/${element.weather[0].icon}.png"> ${(element.main.temp_min).toPrecision(2)}° / ${(element.main.temp_max).toPrecision(2)}°
+        </span>
+      `
+          }
+  
+      })
+  
+      return myDiv
+  }
+
+  // Affichage des 2 derniers jours 
+
+  function createOther(other) {
+    let myDiv = ''
+    let count = 5
+    other.forEach(element => {
+        count--
+        if (count <= 1) {
+            myDiv += `
+             <div class="collapse" id="collapseExample">
+      <span class="day">
+        <p>${moment(element.dt_txt).format('dddd')}</p> <img src="http://openweathermap.org/img/wn/${element.weather[0].icon}.png"> ${(element.main.temp_min).toPrecision(2)}° / ${(element.main.temp_max).toPrecision(2)}°
+      </span>
+      </div>
+    `
+        }
+
+    })
+
+    return myDiv
+}
+  
+
